e92abda78f2d318d4fc870e147e6c4a778b79b73
RAVITEJA104 <teja76450@gmail.com>
2026-09-05 19:27:09 +0530

    demo for git
    (cherry picked from commit a7edfa1d9be87d28f0da23f4cd9a757dfbcdcec1)

---
+++ CharacterCount.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class CharacterCount {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String input = sc.nextLine();
+
+
+        System.out.println(input);
+
+        String[] words = input.split(" ");
+
+        for (String word : words) {
+            char ch = word.charAt(0);
+            int count = word.length();
+
+            System.out.print(ch + "" + count);
+        }
+    }
+}
